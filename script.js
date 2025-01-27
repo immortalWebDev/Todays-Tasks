739a5e675f39d8a92aeabaa5c58362cf9fd2282b
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-27 14:50:58 +0530

    Task check logic (WEP)

---
+++ script.js
@@ -0,0 +1,9 @@
+const checkBoxList = document.querySelectorAll(".custom-checkbox")
+
+checkBoxList.forEach((checkbox) => {
+    checkbox.addEventListener('click',(e) => {
+        checkbox.parentElement.classList.toggle('completed')
+    })
+})
+
+ 
